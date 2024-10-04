fce384831c21ec9a2044f16c659894ba69b5cf09
rgb2hsl <ru.web.designer@gmail.com>
2024-10-04 17:04:15 +0300

    initial

---
+++ index.js
@@ -0,0 +1,80 @@
+const axios = require('axios');
+const fs = require('fs');
+require('dotenv').config();
+let fetch;
+(async () => {
+    fetch = (await import('node-fetch')).default;
+})();
+
+// Настройки
+const URL = 'https://beaconcha.in/api/v1/validator/';
+const VALIDATOR_ID = 'afcd5afb7c7d25ee0a23bb545e8b9c86059279222216a942955ab88feb35506f65323b6364efb37cb2d54ed54ff5efed';
+const BEACONCHAIN_API_KEY = 'QVViMlRYS09GZmQ4ZTcxSG02OXpvTkJ1UTRNUg';
+const TELEGRAM_TOKEN = '7613542993:AAGMFEq6mMGHAPtfpyRWd885qyox2LF8FHg';
+const TELEGRAM_CHAT_ID = '9178664';
+const CHECK_INTERVAL = parseInt(process.env.CHECK_INTERVAL, 10) || 60000;
+
+async function getValidatorData() {
+    try {
+        console.info('Fetching validator data...');
+        const response = await axios.get(`${URL}${VALIDATOR_ID}`, {
+            headers: {
+                'accept': 'application/json',
+                'apikey': BEACONCHAIN_API_KEY
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('ERROR Fetching validator data:', error.response ? error.response.data : error.message);
+        return null;
+    }
+}
+
+async function sendTelegramNotification(message) {
+    const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
+    try {
+        await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                chat_id: TELEGRAM_CHAT_ID,
+                text: message,
+            }),
+        });
+        console.log('Telegram Notification Sent');
+    } catch (error) {
+        console.error('ERROR during Telegram Notification:', error);
+    }
+}
+
+async function monitorBalance() {
+    const validatorData = await getValidatorData();
+    if (!validatorData || validatorData.status !== 'OK') {
+        console.log('ERROR: Validator data format issue');
+        return;
+    }
+
+    const currentBalance = parseFloat(validatorData.data.balance / 1e9);
+
+    console.log(`Fetched balance: ${currentBalance.toFixed(5)} ETH`);
+
+    let previousBalance = '';
+    try {
+        previousBalance = parseFloat(fs.readFileSync('balance.txt', 'utf8'));
+    } catch (error) {
+        console.info('No prev. balance. Create a balance.txt file');
+    }
+
+    if (currentBalance !== previousBalance) {
+        console.log('Balance changed');
+        await sendTelegramNotification(`Balance changed! New balance: ${currentBalance.toFixed(5)} ETH. Previous was ${previousBalance.toFixed(5)} ETH`);
+
+        // Сохранить новое значение баланса
+        fs.writeFileSync('balance.txt', currentBalance.toFixed(5));
+    }
+}
+
+monitorBalance().then(() => {
+    setInterval(monitorBalance, CHECK_INTERVAL);
+})
+
